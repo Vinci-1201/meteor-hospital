28d7100f071d8b0f27a7f09b151854069c663eda
Vinci <117816789+Vinci-1201@users.noreply.github.com>
2023-10-26 12:55:42 +0800

    Footer done
    @Victoria please change whatever u want

---
+++ connection.php
@@ -0,0 +1,19 @@
+<!-- This is the file to open the connection between the PHP files and databses -->
+<?PHP
+
+// Default servername, username, password, and databasename
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "meteor_hospital";
+
+// Open the conection between database and the system
+$condb = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+else{
+    echo "Connected successfully!";
+}
